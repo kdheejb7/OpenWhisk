78b29cd979e9503de9a5e8b5f2dbb85c35d65f6f
kdheejb7 <kdheejb7@naver.com>
2020-03-09 22:12:07 +0900

    example

---
+++ examples/python_example/hello.py
@@ -0,0 +1,9 @@
+def main(dict):
+    if 'name' in dict:
+        name = dict['name']
+    else:
+        name = "stranger"
+    greeting = "Hello " + name + "!"
+    print(greeting)
+    return{"greeting": greeting}
+
